b1506cb504ccee3b2b1cd1e8813ff26c55f20b2f
Jennifer Parra <jparra.biblio@gmail.com>
2019-02-25 17:30:27 -0300

    extrae link y valida los links - falta exportar la función como promesa

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+#!/usr/bin/env node
+
 module.exports = () => {
   // ...
 };
 const fs = require('fs');
 const path = require('path');
 const markdownLinkExtractor = require('markdown-link-extractor'); 
-
+const fetch = require('node-fetch');
 /* let ruta = 'README.md'; 
     path.extname(ruta);
     console.log(path.extname(ruta)); */
-
-
 /* mdLinks("./some/example.md")
 .then(link => {
       // => [{ href, text, file }]
 })
 .catch(console.error); */
+const mdLinks = ()=>{
+const ruta = process.argv[2]; 
 
-
-const ruta = process.argv[2]; //=> es la lista de argumentos que se le entregó al programa. El primero ya corresponde a "posicion 0 -> la dirección de node" "posicion 1 -> el archivo que está ejecutandose"
+//=> es la lista de argumentos que se le entregó al programa. El primero ya corresponde a "posicion 0 -> la dirección de node" "posicion 1 -> el archivo que está ejecutandose"
 var markdown = fs.readFileSync(ruta).toString();
-var links = markdownLinkExtractor(markdown);
-links.forEach(function (link) {
-console.log(link);
-    });
+var links = markdownLinkExtractor(markdown); //markdownLinkExtractor => parametros (Texto en formato markdown) devolcuiones(un array que contiene las URL de los enlaces encontrados).
+for(let i =0; i< links.length; i++){
+   fetch (links[i])
+   .then((res) =>{
+    const objectLinks = {urlLink:`${res.url}`, statusLink:`${res.status}`, statusText:`${res.statusText}`}
+    console.log(objectLinks); 
+})
+   .catch(err => console.error(links[i] + "Link roto"));
+}}
 
+
+module.exports = {mdLinks};
