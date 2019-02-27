894f02c7e931ce5332fbd44ab8111a5c16944673
Jennifer Parra <jparra.biblio@gmail.com>
2019-02-27 16:12:47 -0300

    encerré todo en una función (mdLinks), agregué métodos path.resolve y path.extname

--- index.js
+++ index.js
@@ -15,31 +15,41 @@ const fetch = require("node-fetch");
       // => [{ href, text, file }]
 })
 .catch(console.error); */
-
 const ruta = process.argv[2];
+let absolutPath=path.resolve(ruta);
+const extname = path.extname(absolutPath);
 
+
+const mdLinks = (file) => {
 //=> es la lista de argumentos que se le entregó al programa. El primero ya corresponde a "posicion 0 -> la dirección de node" "posicion 1 -> el archivo que está ejecutandose"
-var markdown = fs.readFileSync(ruta).toString();
-var links = markdownLinkExtractor(markdown);
-const arrayFetch = []; //markdownLinkExtractor => parametros (Texto en formato markdown) devolcuiones(un array que contiene las URL de los enlaces encontrados).
+if(extname == '.md'){
+const markdown = fs.readFileSync(absolutPath).toString();
+const links = markdownLinkExtractor(markdown);//markdownLinkExtractor => parametros (Texto en formato markdown) devolcuiones(un array que contiene las URL de los enlaces encontrados).
+const arrayFetch = []; 
 for (let i = 0; i < links.length; i++) {
+  const text = links[i].text;
   const url = links[i];
-  const a = fetch(links[i])
-    .then(res => {
+  const fetchEachLink = fetch(links[i]) // guardamos todas las promesas[i] en una variable
+    .then((res) => { //si la promesa es resuelta se ejecuta la función que le pasamos.
       const objectLinks = {
-        urlLink: `${res.url}`,
-        statusLink: `${res.status}`,
-        statusText: `${res.statusText}`
+        urlLink: res.url, 
+        text: text,
+        statusLink: res.status,
+        statusText: res.statusText
       };
-      console.log(objectLinks);
-      return objectLinks;
+      return objectLinks; // por qué lo retorno acá?
     })
-    .catch(err => {
-      const objectFail = { urlLink: `${url}`, statusLink: "Fail" };
-      return objectFail;
+    .catch((err) => {
+      const objectFail = { urlLink: url, statusLink: "Fail" };
+      return objectFail; //por qué lo retorno acá?
     });
-  arrayFetch.push(a);
+  arrayFetch.push(fetchEachLink); //ahora colocamos todas las promesas que estaban guardadas en fetchEachLinks en el arreglo vacio que teniamos
 }
-Promise.all(arrayFetch).then(arrRes => {
+Promise.all(arrayFetch).then(arrRes => {      //Hacemos solo UNA PROMESA que contenga el array de promesas.
   console.log(arrRes);
 });
+} else{console.log('No es un archivo markdown')
+}
+}
+mdLinks(absolutPath)
+
